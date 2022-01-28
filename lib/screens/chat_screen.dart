9a8962984835c4b10eea877a4aeb548a06e1ee61
Mahmoud Muhammad <mahmoudmuhammadelg@gmail.com>
2022-01-28 09:27:00 +0200

    Initial commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  static const String id='chat_screen';
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _firestore=FirebaseFirestore.instance;
+  final _auth=FirebaseAuth.instance;
+  User loggedInUser;
+  String messageText;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser(){
+    try{
+      final user=  _auth.currentUser;
+      if(user!=null){
+        loggedInUser=user;
+      }
+    }
+    catch(e){
+      print(e);
+    }
+  }
+  void getMessages()async{
+    final messages= await _firestore.collection('messages').get();
+    for(var message in messages.docs){
+      print(message.data());
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                //Implement logout functionality
+                // _auth.signOut();
+                // Navigator.pop(context);
+                getMessages();
+              }),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        //Do something with the user input.
+                        messageText=value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //Implement send functionality.
+                      _firestore.collection('messages').add({
+                        'text':messageText,
+                        'sender':loggedInUser.email
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
